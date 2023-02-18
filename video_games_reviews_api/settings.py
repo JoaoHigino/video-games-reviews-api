f400ee61e1698c73f413e07b8d7666e607e777ba
JoaoHigino <jhigino10@hotmail.com>
2023-02-18 13:43:06 +0000

    Add favourites to settings.py and urls.py

--- video_games_reviews_api/settings.py
+++ video_games_reviews_api/settings.py
@@ -97,6 +97,7 @@ INSTALLED_APPS = [
     'likes',
     'followers',
     'contacts',
+    'favourites',
 ]
 SITE_ID = 1
 MIDDLEWARE = [
--- video_games_reviews_api/urls.py
+++ video_games_reviews_api/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('', include('likes.urls')),
     path('', include('followers.urls')),
     path('', include('contacts.urls')),
+    path('', include('favourites.urls')),
 ]
