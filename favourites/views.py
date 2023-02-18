a7edd449fc0da2c7115ebf8456907a36001fee46
JoaoHigino <jhigino10@hotmail.com>
2023-02-18 13:42:00 +0000

    Create favourites and add models, serializer, urls and apps on it

---
+++ favourites/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics, permissions
+from video_games_reviews_api.permissions import IsOwnerOrReadOnly
+from favourites.models import Favourite
+from favourites.serializers import FavouriteSerializer
+
+
+class FavouriteList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FavouriteSerializer
+    queryset = Favourite.objects.all()
+
+    def perform_create(self, serializer):
+       
+        serializer.save(owner=self.request.user)
+
+
+class FavouriteDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FavouriteSerializer
+    queryset = Favourite.objects.all()
